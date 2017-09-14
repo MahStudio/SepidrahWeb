d65e3ad4955cdbe4bc48a14e9935eb49231ccea4
@Mohsens22 <mohsens2@outlook.com>
2017-09-15 01:37:44 +0430

    Added utils and some other stuff

---
+++ Sepidrah.Web/Sepidrah.Web.Core/Models/UserEmailLookupModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sepidrah.Web.Core.Models
+{
+    class UserEmailLookupModel
+    {
+        private string _type = "user";
+        private int _schemaver = 0;
+
+        public string Type { get { return _type; } }
+        public int Key { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int SchemaVer { get { return _schemaver; } }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Sepidrah.Web/Sepidrah.Web.Core/Util/Utils.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sepidrah.Web.Core.Util
+{
+    public static class Utils
+    {
+        internal static string KeyMaker(string type, int key)
+        {
+            return type + "::" + key;
+        }
+        internal static string KeyMaker(string type, string key)
+        {
+            return type + "::" + key;
+        }
+        internal static string KeyMaker(string type, int key, string suffix)
+        {
+            return type + "::" + key + "_" + suffix;
+        }
+
+        public static string CalculateHash(string pass)
+        {
+
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+
+                return String.Concat(hash
+                  .ComputeHash(Encoding.UTF8.GetBytes(pass))
+                  .Select(item => item.ToString("x2")));
+
+            }
+        }
+        public static long ToUnixTime(DateTime dateTime)
+        {
+            return (int)(dateTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static string ToBase64(string Path)
+        {
+
+            using (Image image = Image.FromFile(Path))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
+    }
+}
